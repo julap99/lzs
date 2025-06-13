33f3676661560c720dfbffebc5313851de435a98
Haqeem Solehan <93633646+julap99@users.noreply.github.com>
2025-06-14 00:34:08 +0800

    Set SSR to false in nuxt.config.js to disable server-side rendering.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     auth: {
       secretAccess: process.env.NUXT_ACCESS_TOKEN_SECRET,
