46969440f959d8fb9b79501fd4b1a398968809bf
nasih-ulwan <1211202721@student.mmu.edu.my>
2025-09-23 01:04:31 +0800

    [BF-BTN] Update button labels and expand JSON data structure for aid categories
    - Changed button label from 'Format Maklumat' to 'Template Format Excel' for clarity.
    - Expanded JSON data structure to include additional aid categories and subcategories for disaster assistance, enhancing the application form's data handling capabilities.

--- pages/BF-BTN/bantuan-bulk/cipta-bantuan-bulk/[id]/edit.vue
+++ pages/BF-BTN/bantuan-bulk/cipta-bantuan-bulk/[id]/edit.vue
@@ -217,7 +217,7 @@
               @click="downloadPayableToTemplate"
             >
               <Icon name="material-symbols:download" class="mr-1" />
-              Format Maklumat
+              Template Format Excel
             </rs-button>
             </div>
             
