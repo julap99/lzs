e6d3fb789600378be2add2f22280c1a3446009f1
AlepIman21 <alifsarimah02@gmail.com>
2025-09-26 17:03:48 +0800

    BF-ADN-PA-DA-01 feat BF-ADN-PA-DA-06 feat BF-ADN-PA-AT-02_1 feat ADN-PA-AT-03_1 feat ADN-PA-AT-09_1 feat ADN-PA-AT-10_1: Refactor navigation structure and update iframe templates for complaint forms

---
+++ pages/BF-ADN/PA/AT/10/ADN-PA-AT-10_1/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <iframe 
+  src="https://bp.corrad.ai/workflow/4b39c4fe-a052-48cb-a655-f7a442278365?debug=false&hideComplete=true&theme=light"
+  width="100%" 
+  height="600px"
+  style="border: none; border-radius: 8px;">
+</iframe>
+</template>
